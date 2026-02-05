8bd8f2abea821969552d714374acb60f02f3fd93
ulinycoin <ulinycoin7@gmail.com>
2026-02-05 17:56:23 +0200

    Build privacy/terms pages for Pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/goldenko/'
+  base: '/goldenko/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+        terms: resolve(__dirname, 'terms.html')
+      }
+    }
+  }
 })
